c2ad858d170e2416841dd418eecde26f4004fabd
AnnaLikhanskaya <a.lihansky@yandex.ru>
2025-01-06 19:40:51 +0300

    ДП этап 3

--- stats-service/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ stats-service/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -1,11 +1,8 @@
 package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication()
 public class StatsServer {
